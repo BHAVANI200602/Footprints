40b273a244077385a123a3ca42994067b8d49c95
BHAVANI200602 <ratgrey73@gmail.com>
2026-05-22 18:20:06 +0530

    version 1.1.0

--- server.ts
+++ server.ts
@@ -158,7 +158,7 @@ async function fetchLeetcode(username: string) {
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = Number(process.env.PORT) || 3000;
 
   app.use(express.json());
 
